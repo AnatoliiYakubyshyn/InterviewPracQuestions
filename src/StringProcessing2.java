384e70a4910e9b2ad2eef107825d51499b590dd3
AnatoliiYakubyshyn <ayakubyshyn@solvd.com>
2024-07-01 10:36:27 +0300

    implemented task2

---
+++ src/StringProcessing2.java
@@ -0,0 +1,9 @@
+public class StringProcessing2 {
+    public static boolean isTwoStringsPalindromes(String a, String b) {
+        if (a.length()!=b.length()) {
+            return false;
+        }
+        b = new StringBuilder(b).reverse().toString();
+        return a.hashCode() == b.hashCode() && a.equals(b);
+    }
+}
---
+++ src/StringProcessing2Test.java
@@ -0,0 +1,8 @@
+public class StringProcessing2Test {
+
+    public static void main(String[] args) {
+        System.out.println(StringProcessing2.isTwoStringsPalindromes("ab","ba"));
+        System.out.println(StringProcessing2.isTwoStringsPalindromes("abc","ba"));
+        System.out.println(StringProcessing2.isTwoStringsPalindromes("ab","aa"));
+    }
+}
