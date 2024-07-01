ff8f3865d558956fa882e3819162563e32f92b99
AnatoliiYakubyshyn <ayakubyshyn@solvd.com>
2024-07-01 12:37:27 +0300

    implemented calculation in matrix task

---
+++ src/CalculationInMatrixAdvanced.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculationInMatrixAdvanced {
+
+    public static List<int[]> getRectangle(int[][] matrix) {
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        List<int[]> res = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0 && !visited[i][j]) {
+                    int[] innerAns = new int[4];
+                    innerAns[0] = i;
+                    innerAns[1] = j;
+                    int k = i;
+                    while (k < matrix.length && matrix[k][j] == 0) {
+                        k++;
+                    }
+                    int i2 = k - 1;
+                    k = j;
+                    while (k < matrix[0].length && matrix[i][k] == 0) {
+                        k++;
+                    }
+                    int j2 = k - 1;
+                    innerAns[2] = i2;
+                    innerAns[3] = j2;
+                    res.add(innerAns);
+                    for (int o = innerAns[0]; o <= innerAns[2]; o++) {
+                        for (int r = innerAns[1]; r <= innerAns[3]; r++) {
+                            visited[o][r] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ src/CalculationMatrixTest.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class CalculationMatrixTest {
+
+    public static void printRes(List<int[]> res) {
+        for (int[] a : res) {
+            System.out.println(a[0] + " " + a[1] + " " + a[2] + " " + a[3]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] image = new int[][]{
+                {1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 0, 0, 0, 1},
+                {1, 1, 1, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1}
+        };
+        List<int[]> res = CalculationInMatrixAdvanced.getRectangle(image);
+        System.out.println("TEST CASE#1:");
+        printRes(res);
+
+        int[][] image2 = new int[][]{
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 0, 0, 0, 1, 1, 0, 1},
+                {1, 1, 1, 0, 0, 0, 1, 1, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+
+        };
+        System.out.println("TEST CASE#2:");
+        printRes(CalculationInMatrixAdvanced.getRectangle(image2));
+    }
+}
