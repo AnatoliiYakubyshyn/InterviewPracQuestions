be2ea63d4358eed32f6a764e44d5ea6ba2fff5a2
Anatolii Yakubyshyn <ayakubyshyn@solvd.com>
2024-07-04 11:22:20 +0300

    implemented unit test

---
+++ src/main/java/com/solvd/practicalInterviewQuestions/MapProcessing.java
@@ -0,0 +1,10 @@
+package com.solvd.practicalInterviewQuestions;
+
+import java.util.Map;
+
+public class MapProcessing {
+
+    public static int getTotalNumberOfViewForTheLastTwoDays(Map<String, Map<String, Map<String, Integer>>> viewsParams) {
+        throw new RuntimeException("Not implemented");
+    }
+}
